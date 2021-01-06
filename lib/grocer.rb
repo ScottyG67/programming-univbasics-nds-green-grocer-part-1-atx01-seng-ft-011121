5f9700c0773dd7ee5791e12cae9e1d58622d6e45
Scott Gloyna <scott.gloyna01@gmail.com>
2021-01-06 21:06:37 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -20,6 +20,7 @@ def consolidate_cart(cart)
   #cart is AoH, each hash has 3 attributes, :item, :price, :clearance
   #need to return AoH, each hash has 4 attributes, :item,:price, :clearance (from cart) and :count which is the number of times this hash was in cart
   require 'pry'
+  organized_cart 
   cart.each do |index|
     index[:item]
     binding.pry
