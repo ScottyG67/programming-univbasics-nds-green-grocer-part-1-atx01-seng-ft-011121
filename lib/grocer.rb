c798393fdf8b56d50e782bb879203c11b0d2b6cf
Scott Gloyna <scott.gloyna01@gmail.com>
2021-01-06 20:48:53 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,6 +2,12 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  #collection is AoH, each hash has 3 attributes, :item, :price, :clearance
+  #name is String and corrliates to :item in collection
+  #need to return full hash with :item==name and nill if no match
+  
+  collection.each_with_object |index| 
+  
 
 end
 
