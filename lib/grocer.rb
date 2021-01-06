12c95aa70dbee9f6b403be2b7a6b75e5488b82b1
Scott Gloyna <scott.gloyna01@gmail.com>
2021-01-06 21:23:00 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,13 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  
+  #collection is AoH, each hash has 3 attributes, :item, :price, :clearance
+  #name is String and corrliates to :item in collection
+  #need to return full hash with :item==name and nill if no match
+  require 'pry'
 
+  item_hash=collection.find {|index| index[:item]==name}
 end
 
 def consolidate_cart(cart)
@@ -10,7 +16,18 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+  
+  #cart is AoH, each hash has 3 attributes, :item, :price, :clearance
+  #need to return AoH, each hash has 4 attributes, :item,:price, :clearance (from cart) and :count which is the number of times this hash was in cart
+  require 'pry'
+  organized_cart=[]
+  count = 0
+  cart.each do |index|
+    name = index[:item]
+    #use find_all to produce array with all hashes of desired item. lenght then converts array into integer of number of items.
+    index[:count] = cart.find_all{|i| i[:item]==name}.length
+    organized_cart << index
+  end
 end
 
 
