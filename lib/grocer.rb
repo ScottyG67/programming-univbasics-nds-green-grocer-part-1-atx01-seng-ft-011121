3207bb41b1ee34c0325bdd8132643e3e776f5f99
Scott Gloyna <scott.gloyna01@gmail.com>
2021-01-06 20:54:35 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -11,7 +11,7 @@ def find_item_by_name_in_collection(name, collection)
   item_hash=collection.find do |index|
     binding.pry
     index[:item]==name
-    binding.pry
+    #binding.pry
   end
   binding.pry
 
